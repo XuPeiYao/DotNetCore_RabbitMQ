32c27e39202d1b388d40ad9f05357a33e1776ee9
XuPeiYao <st89382000@gmail.com>
2019-06-23 18:00:29 +0800

    Init

---
+++ FFMpegConverter/Program.cs
@@ -0,0 +1,55 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
+using System.Threading;
+using XWidget.FFMpeg;
+
+namespace FFMpegConverter {
+    class Program {
+        static void Main(string[] args) {
+            var sdConverter = new FFMpegConverterBuilder()
+                .SetExecutePath(@"D:\FFMpeg-Runtime\bin\ffmpeg.exe")
+                .ConfigVideo(v => {
+                    v.SetSize(CommonSize.SD);
+                })
+                .Build();
+
+            var factory = new ConnectionFactory() { HostName = "192.168.1.2" };
+            var connection = factory.CreateConnection();
+            var channel = connection.CreateModel();
+
+            var consumer = new EventingBasicConsumer(channel);
+            consumer.Received += (model, ea) => {
+                var path = Encoding.UTF8.GetString(ea.Body);
+                var temp_outputPath = @"D:\OutputFiles\" + Guid.NewGuid() + ".mp4";
+
+                sdConverter.Convert(path, temp_outputPath)
+                    .Subscribe(
+                        (ConvertResult result) => {
+                            channel.BasicAck(ea.DeliveryTag, false); // 任務處理完畢，剔除Queue
+
+                            if (result.ExitCode == 0) {
+                                // 送出轉檔成功訊息
+                                channel.BasicPublish(string.Empty, "convert-success", null, Encoding.UTF8.GetBytes(temp_outputPath));
+                            } else {
+                                // 送出轉檔失敗訊息
+                                channel.BasicPublish(string.Empty, "convert-fail", null, Encoding.UTF8.GetBytes(path));
+                            }
+                        }, (Exception e) => {
+                            channel.BasicAck(ea.DeliveryTag, false); // 任務處理完畢，剔除Queue
+
+                            // 送出轉檔失敗訊息
+                            channel.BasicPublish(string.Empty, "convert-fail", null, Encoding.UTF8.GetBytes(path + Environment.NewLine + e.ToString()));
+                        });
+            };
+
+            channel.BasicConsume(
+                queue: "convert",
+                autoAck: false,
+                consumer: consumer);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ RabbitMQ_Test/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Text;
+using System.Threading;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace RabbitMQ_Test {
+    class Program {
+        static void Main(string[] args) {
+            var factory = new ConnectionFactory() { HostName = "192.168.1.2" };
+
+            var connection = factory.CreateConnection();
+
+            var channel = connection.CreateModel();
+
+            channel.QueueDeclare(
+                    queue: "convert",
+                    durable: true, // 持久性，確保RMQ出問題可以繼續存在
+                    exclusive: false, // 不獨佔
+                    autoDelete: false, // 不自動清除
+                    arguments: null);
+            channel.QueueDeclare(
+                    queue: "convert-success",
+                    durable: true, // 持久性，確保RMQ出問題可以繼續存在
+                    exclusive: false, // 不獨佔
+                    autoDelete: false, // 不自動清除
+                    arguments: null);
+            channel.QueueDeclare(
+                    queue: "convert-fail",
+                    durable: true, // 持久性，確保RMQ出問題可以繼續存在
+                    exclusive: false, // 不獨佔
+                    autoDelete: false, // 不自動清除
+                    arguments: null);
+
+            var convertSuccess = new EventingBasicConsumer(channel);
+            convertSuccess.Received += (model, ea) => {
+                var path = Encoding.UTF8.GetString(ea.Body);
+                Console.WriteLine("轉檔成功: " + path);
+            };
+
+            var convertFail = new EventingBasicConsumer(channel);
+            convertFail.Received += (model, ea) => {
+                var message = Encoding.UTF8.GetString(ea.Body);
+                Console.WriteLine("轉檔失敗: " + message);
+            };
+
+
+            channel.BasicConsume(
+                queue: "convert-success",
+                autoAck: true, // 自動ACK
+                consumer: convertSuccess);
+            channel.BasicConsume(
+                queue: "convert-fail",
+                autoAck: true, // 自動ACK
+                consumer: convertFail);
+
+            channel.BasicPublish(string.Empty, "convert", null, Encoding.UTF8.GetBytes(@"D:\不存在檔案.mp4"));
+            channel.BasicPublish(string.Empty, "convert", null, Encoding.UTF8.GetBytes(@"D:\SampleFiles\SampleVideo_1280x720_10mb.mp4"));
+
+            Console.ReadKey();
+        }
+    }
+}
